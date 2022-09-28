8293323ab56007a393189bd9a6d5d11060e2fa16
juniorMira <j-unyor14@hotmail.com>
2022-09-28 13:37:49 -0300

    create model matches

---
+++ app/backend/src/database/models/MatchesModel.ts
@@ -0,0 +1,42 @@
+import { BOOLEAN, Model, INTEGER } from 'sequelize';
+import db from '.';
+
+class MatchesModel extends Model {
+  id: number;
+  homeTeam: number;
+  homeTeamGoals: number;
+  awayTeam: number;
+  awayTeamGoals: number;
+  inProgress: boolean;
+}
+
+MatchesModel.init({
+  id: {
+    type: INTEGER,
+    primaryKey: true,
+    allowNull: false,
+    autoIncrement: true,
+  },
+  homeTeam: {
+    type: INTEGER,
+  },
+  homeTeamGoals: {
+    type: INTEGER,
+  },
+  awayTeam: {
+    type: INTEGER,
+  },
+  awayTeamGoals: {
+    type: INTEGER,
+  },
+  inProgress: {
+    type: BOOLEAN,
+  },
+}, {
+  sequelize: db,
+  modelName: 'matches',
+  timestamps: false,
+  underscored: true,
+});
+
+export default MatchesModel;
