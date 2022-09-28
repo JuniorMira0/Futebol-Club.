bf543211f13e626f3cf460df202805904ef77634
juniorMira <j-unyor14@hotmail.com>
2022-09-28 13:33:35 -0300

    create model user

---
+++ app/backend/src/database/models/UsersModel.ts
@@ -0,0 +1,40 @@
+import { DataTypes, Model } from 'sequelize';
+import db from '.';
+
+export default class Users extends Model {
+  id: number;
+  username: string;
+  role: string;
+  email: string;
+  password: string;
+}
+
+Users.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  role: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'users',
+  timestamps: false,
+});
